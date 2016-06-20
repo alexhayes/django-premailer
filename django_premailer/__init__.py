6ae75546756743d0d0a0d3850c520047a03e1563
Alex Hayes <alex@common.io>
2016-06-21 07:37:35 +1000

    Updated /home/ahayes/workspace/django-premailer/VERSION and /home/ahayes/workspace/django-premailer/CHANGELOG.md

--- django_premailer/__init__.py
+++ django_premailer/__init__.py
@@ -10,7 +10,7 @@ version_info_t = namedtuple(
     'version_info_t', ('major', 'minor', 'micro', 'releaselevel', 'serial'),
 )
 
-VERSION = version_info_t(0, 1, 0, '', '')
+VERSION = version_info_t(0, 2, 0, '', '')
 __version__ = '{0.major}.{0.minor}.{0.micro}{0.releaselevel}'.format(VERSION)
 __author__ = 'Alex Hayes'
 __contact__ = 'alex@alution.com'
