4b0661548d2e566287e8d641ba5ec0736bd788b0
Alex Hayes <alex@common.io>
2016-06-21 07:12:10 +1000

    Ensure HTML is prettified to remove whitespace differences to get around issues in travis/circleci.

--- django_premailer/tests/test_templatetags.py
+++ django_premailer/tests/test_templatetags.py
@@ -3,12 +3,11 @@ from __future__ import absolute_import
 import django
 import os
 
-from django.test import TestCase
-from django.test.utils import override_settings
-from django.conf import settings
-
 from django.template import Context
 from django.template.loader import get_template
+from django.test import TestCase
+from django.test.utils import override_settings
+from lxml import etree, html
 
 TESTS_ROOT = os.path.dirname(os.path.abspath(__file__))
 TESTS_TEMPLATE_DIR = os.path.join(TESTS_ROOT, 'templates')
@@ -19,10 +18,21 @@ TESTS_TEMPLATE_DIRS = [TESTS_TEMPLATE_DIR]
                    PREMAILER_OPTIONS=dict(base_url='http://example.com'))
 class PremailerTests(TestCase):
 
+    def pretty(self, ugly):
+        """
+        Force a HTML document to be made pretty.
+
+        NOTE: This method is required because of strange differences in the way premailer/lxml makes decisions about
+        the placement of newlines. Using tox all tests pass without this hack, however in travis and circleci the
+        :py:meth:`.PremailerTests.test_basic` test fails due to some missing ``\n`` in the generated ``actual`` HTML.
+        """
+        document_root = html.fromstring(ugly.replace("\n", "").replace("\t", "").replace("  ", ""))
+        return etree.tostring(document_root, encoding='unicode', pretty_print=True)
+
     def assert_expected(self, actual, expected):
         with open(os.path.join(TESTS_TEMPLATE_DIR, expected), 'r') as f:
             expected = f.read()
-            self.assertEqual(actual, expected)
+            self.assertEqual(self.pretty(actual), self.pretty(expected))
 
     def test_basic(self):
         """
